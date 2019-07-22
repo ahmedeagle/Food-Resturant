35e9c800601341abaf10c3591f3422cbb8a41a7d
= <=>
2019-07-22 19:05:44 +0300

    =updated

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -11,7 +11,8 @@ class AuthServiceProvider extends ServiceProvider
      * The policy mappings for the application.
      *
      * @var array
-     *//
+     */
+    //
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
     ];
