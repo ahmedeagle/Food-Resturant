41c13cff60408fe43d46daf9ae6b1115fafaed89
= <=>
2019-08-27 23:11:59 +0300

    =updated

--- resources/views/Site/layouts/pure-master.blade.php
+++ resources/views/Site/layouts/pure-master.blade.php
@@ -49,7 +49,7 @@
                 height:     100%;
                 width:      100%;
                 background: rgba( 255, 255, 255, .8 ) 
-                            url('http://i.stack.imgur.com/FhHRx.gif') 
+                            url('https://i.stack.imgur.com/FhHRx.gif') 
                             50% 50% 
                             no-repeat;
             }
