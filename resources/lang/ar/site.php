b75361d36aaff24f513279fbc13718c93aa1f1ed
= <=>
2019-08-29 16:44:57 +0300

    =updated

--- resources/lang/ar/site.php
+++ resources/lang/ar/site.php
@@ -314,7 +314,7 @@ return [
     "optionsinenglish"                  => "الاضافات بالانجليزية ",
     "addsinarabic"                     => "التفضيلات بالعربية ",
     "addsinenglish"                      => "التفضيلات بالانجليزية ",
-    "congestion"                        => "جالة الازدحام ",
+    "congestion"                        => "حالة الازدحام ",
 
 
  
