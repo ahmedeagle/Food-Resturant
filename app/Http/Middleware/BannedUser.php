5ef4821dae8fdddbee3218a7e22ef3bfc1eca551
= <=>
2019-08-30 16:52:15 +0300

    =updated

---
+++ app/Http/Middleware/BannedUser.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use DB;
+use Illuminate\Support\Facades\Auth;
+ 
+class BannedUser
+{
+    /**
+     * @param $request
+     * @param Closure $next
+     * @param null $guard
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if (Auth::check()) {
+            // Block access if user is banned
+            if (Auth::guard($guard)->user()->blocked == '1') {
+                if ($request->ajax() || $request->wantsJson()) {
+                    return response(trans('messages.user.blocked'), 401);
+                } else {
+                    Auth::logout();
+                    
+                    $message = "This user has been banned.";
+                    
+                    flash()->error($message);
+                    //Alert::error($message)->flash();
+                    
+                    return redirect()->guest('login');
+                }
+            }
+        }
+        
+        return $next($request);
+    }
+}
