7e3993b8d24c189a5358386ef0dbb103edada3af
= <=>
2019-08-30 15:21:21 +0300

    =updated

--- app/Http/Controllers/Admin/Customers.php
+++ app/Http/Controllers/Admin/Customers.php
@@ -133,7 +133,7 @@ class Customers extends Controller {
             
            
              if($validator->fails()){
-                    $error = $validator->errors()->first();
+                 return    $error = $validator->errors()->first();
                       return redirect()->back()->with('errors',$error);
                 }
 
