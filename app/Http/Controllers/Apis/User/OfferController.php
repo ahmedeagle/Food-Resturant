02cc1ecb1a4db4c28c52bcdcf0a47f85c733192c
ahmed emam <ahmed.emam@wisyst.com>
2020-07-06 17:51:48 +0200

    =

--- app/Http/Controllers/Apis/User/OfferController.php
+++ app/Http/Controllers/Apis/User/OfferController.php
@@ -5,37 +5,40 @@ namespace App\Http\Controllers\Apis\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use DB;
+
 class OfferController extends Controller
 {
     // get all offers
-    public function get_offers(Request $request){
+    public function get_offers(Request $request)
+    {
         (new BaseConroller())->setLang($request);
-        $name   = (App()->getLocale() == 'ar') ? 'ar' : 'en';
+        $name = (App()->getLocale() == 'ar') ? 'ar' : 'en';
         $offers = DB::table("offers")
-                    ->join("images", "images.id" , "offers.image_id")
-                    ->join("providers", "providers.id" , "offers.provider_id")
-                     ->join("branches", "providers.id" , "branches.provider_id")
-                     ->join("offers_branches","branches.id","offers_branches.branch_id")
-                    ->where("offers.approved" , "1")
-                    ->select(
-                        "offers.provider_id",
-                        "branches." .$name . "_name AS title",
-                        "offers.".$name."_notes AS notes",
-                        "branches.".$name."_address AS address",
-                        "providers.".$name."_name AS restaurant_name",
-                        "branches.id AS branch_id",
-                        "offers.lft",
-                        DB::raw("CONCAT('". url('/') ."','/storage/app/public/offers/', images.name) AS image_url"),
-                        "providers.accept_order"
-                    )
-                    ->orderBy("offers.lft")
-                    ->paginate(10);
-      $offers = (new HomeController)->filter_offers_branches($request,$name,$offers);
+            ->join("images", "images.id", "offers.image_id")
+            ->join("providers", "providers.id", "offers.provider_id")
+            ->join("branches", "providers.id", "branches.provider_id")
+            ->join("offers_branches", "branches.id", "offers_branches.branch_id")
+            ->where("offers.approved", "1")
+            ->select(
+                "offers.provider_id",
+                "offers.id as offer_id",
+                "branches." . $name . "_address AS address",
+                "branches.id AS branch_id",
+                "offers.lft",
+                "offers." . $name . "_title AS title",
+                "branches." . $name . "_name AS restaurant_name",
+                "offers." . $name . "_notes AS notes",
+                DB::raw("CONCAT('" . url('/') . "','/storage/app/public/offers/', images.name) AS image_url"),
+                "providers.accept_order"
+            )
+            ->orderBy("offers.lft")
+            ->paginate(10);
+        $offers = (new HomeController)->filter_offers_branches($request, $name, $offers);
         return response()->json([
-            "status"      => true ,
-            "errNum"      => 0,
-            "msg"         => trans("messages.success"),
-            "offers"      => $offers
+            "status" => true,
+            "errNum" => 0,
+            "msg" => trans("messages.success"),
+            "offers" => $offers
         ]);
     }
 }
